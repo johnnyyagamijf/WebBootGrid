4216d92b6577a8520c46bcd0ca030d00cf5457f4
John Campos <johnmdcampos@outlook.com>
2020-07-08 09:27:57 -0300

    'pc'

---
+++ WebBootGrid/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebBootGrid.Models;
+
+namespace WebBootGrid.Controllers
+{
+    public class HomeController : Controller
+    {
+        private List<Produtos> Produtos { get; set; }
+        public HomeController()
+        {
+            Produtos = new List<Produtos>();
+        }
+        public IActionResult Index()
+        {
+            Produtos.Add(new Produtos { ID = 1, Link = "Madalena@shshlsdahldashlka.com", Received = "No", Sender = "sim" });
+
+            Produtos.Add(new Produtos { ID = 2, Link = "john@shshlsdahldashlka.com", Received = "No", Sender = "sim" });
+            Produtos.Add(new Produtos { ID = 3, Link = "Tatiane@shshlsdahldashlka.com", Received = "Sim", Sender = "No" });
+            Produtos.Add(new Produtos { ID = 4, Link = "Davi@shshlsdahldashlka.com", Received = "Sim", Sender = "No" });
+            Produtos.Add(new Produtos { ID = 1, Link = "Madalena@shshlsdahldashlka.com", Received = "No", Sender = "sim" });
+
+            Produtos.Add(new Produtos { ID = 2, Link = "john@shshlsdahldashlka.com", Received = "No", Sender = "sim" });
+            Produtos.Add(new Produtos { ID = 3, Link = "Tatiane@shshlsdahldashlka.com", Received = "Sim", Sender = "No" });
+            Produtos.Add(new Produtos { ID = 4, Link = "Davi@shshlsdahldashlka.com", Received = "Sim", Sender = "No" });
+            Produtos.Add(new Produtos { ID = 1, Link = "Madalena@shshlsdahldashlka.com", Received = "No", Sender = "sim" });
+
+            Produtos.Add(new Produtos { ID = 2, Link = "john@shshlsdahldashlka.com", Received = "No", Sender = "sim" });
+            Produtos.Add(new Produtos { ID = 3, Link = "Tatiane@shshlsdahldashlka.com", Received = "Sim", Sender = "No" });
+            Produtos.Add(new Produtos { ID = 4, Link = "Davi@shshlsdahldashlka.com", Received = "Sim", Sender = "No" });
+            Produtos.Add(new Produtos { ID = 1, Link = "Madalena@shshlsdahldashlka.com", Received = "No", Sender = "sim" });
+
+            Produtos.Add(new Produtos { ID = 2, Link = "john@shshlsdahldashlka.com", Received = "No", Sender = "sim" });
+            Produtos.Add(new Produtos { ID = 3, Link = "Tatiane@shshlsdahldashlka.com", Received = "Sim", Sender = "No" });
+            Produtos.Add(new Produtos { ID = 4, Link = "Davi@shshlsdahldashlka.com", Received = "Sim", Sender = "No" });
+
+            return View(Produtos);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebBootGrid/Models/Produtos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebBootGrid.Models
+{
+    public class Produtos
+    {
+        public int ID { get; set; }
+        public string Sender { get; set; }
+        public string Received { get; set; }
+        public string Link { get; set; }
+    }
+}
